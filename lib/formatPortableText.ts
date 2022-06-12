f2005af8f105053f5f78e6a7db809ae207d07a4d
kiosion <hi@kio.dev>
2022-06-12 18:21:46 -0300

    update format

--- lib/formatPortableText.ts
+++ lib/formatPortableText.ts
@@ -1,7 +1,19 @@
-// import { Gemtext } from '';
+import { 
+  Gemtext,
+  LineText,
+  LinePreformattingToggle,
+  LineHeading,
+  LineQuote,
+  LineListItem
+} from 'https://deno.land/x/kaksik/mod.ts';
 
-export const format = (sanityRes: any): Promise<string[]> => new Promise ((resolve, reject) => {
+export const format = (sanityRes: any): Promise<Gemtext> => new Promise (async (resolve, reject) => {
   // TODO
   // Format sanity response w/ portabletext into an array of Gemtext-formatted strings
+  let content = new Gemtext();
+
+  // ...
+
+  resolve(content);
 });
 
