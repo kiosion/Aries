657150f941af617f7aef29af9d3295fbfe5ca80f
kiosion <hi@kio.dev>
2022-06-11 21:47:38 -0300

    add fetch.ts

--- lib/client.ts
+++ lib/client.ts
@@ -1,12 +1,10 @@
 import { createRequire } from 'https://deno.land/std/node/module.ts';
-import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 const require = createRequire(import.meta.url);
 const sanityClient = require('@sanity/client');
 const imageBuilder = require('@sanity/image-url');
 
 export const client = sanityClient({
-  // projectId: config.SANITY_PROJECT_ID,
   projectId: Deno.env.get("SANITY_PROJECT_ID"),
   dataset: 'production',
   apiVersion: '2022-05-04',
