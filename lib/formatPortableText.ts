09deb00b43bbba5447b5de6b58fc1e63544ca536
kiosion <hi@kio.dev>
2022-06-12 18:17:46 -0300

    create formatPortableText.ts

---
+++ lib/formatPortableText.ts
@@ -0,0 +1,7 @@
+// import { Gemtext } from '';
+
+export const format = (sanityRes: any): Promise<string[]> => new Promise ((resolve, reject) => {
+  // TODO
+  // Format sanity response w/ portabletext into an array of Gemtext-formatted strings
+});
+
